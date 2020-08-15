98b945e47078e8cc1623dc0fe31e97f5a73702d4
DR2020ztt <1006265542@qq.com>
2020-08-15 16:06:09 +0800

    8.15

---
+++ src/com/neusoft/elm/view/BusinessView.java
@@ -0,0 +1,16 @@
+package com.neusoft.elm.view;
+
+import com.neusoft.elm.po.Business;
+
+public interface BusinessView {
+
+	public void listBusiness();
+	public void removeBusinessById();
+	public void listBusinessByLike();
+	public void saveBusiness();
+	
+	public Business login();
+	public Business getBusinessById(Integer businessId);
+	public void updateBusiness(Integer businessId);
+	public boolean updateBusinessPassword(Integer businessId);
+}
---
+++ src/com/neusoft/elm/view/FoodView.java
@@ -0,0 +1,13 @@
+package com.neusoft.elm.view;
+
+import java.util.List;
+
+import com.neusoft.elm.po.Food;
+
+public interface FoodView {
+
+	public List<Food> listFoodByBusinessId(Integer businessId);
+	public void saveFood(Integer businessId);
+	public void removeFood(Integer businessId);
+	public boolean updateFood(Integer businessId);
+}
